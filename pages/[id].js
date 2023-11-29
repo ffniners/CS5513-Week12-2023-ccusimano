60d706bcd0cc36ef40fc5180e8cc7c07bb0d5d00
ffniners <125220687+ffniners@users.noreply.github.com>
2023-11-29 03:15:43 +0000

    Initial commit

---
+++ pages/[id].js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+import { getAllIds, getData } from '../lib/data';
+
+export async function getStaticProps({ params }) {
+  const itemData = await getData(params.id);
+  // console.log(itemData);
+  return {
+    props: {
+      itemData
+    }
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllIds();
+  return {
+    paths,
+    fallback: false
+  };
+}
+
+export default function Entry({ itemData }) {
+  return (
+    <Layout>
+      <article className="card col-6">
+        <div className="card-body">
+          {console.log(itemData.id)}
+          <h5 className="card-title">{itemData.id}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{itemData.post_title}</h6>
+          <p className="card-text">{itemData.post_title}</p>
+        </div>
+      </article>
+    </Layout>
+  );
+}
+
+
+/*
+<a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a> */ 
